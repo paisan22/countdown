1bea8e4e5170007ab602f7bee6983f596eb6073c
paisan <paisan.rietbroek@gmail.com>
2018-03-19 13:31:00 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by paisanrietbroek on 19/03/2018.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
